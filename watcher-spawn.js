97c773279ad6d2c31b7fbbc4633dc6b3a30733cc
Jeff Steed <steed.jeff@gmail.com>
2018-04-12 07:09:23 -0500

    initial commit

---
+++ watcher-spawn.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const fs = require('fs');
+const spawn = require('child_process').spawn;
+const filename = process.argv[2];
+
+if (!filename) {
+  throw Error('A file to watch must be specified!');
+}
+
